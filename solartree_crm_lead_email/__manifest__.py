5315e9a6c347d1a0ab46ac86e5cefb1f7cc1d206
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-11-06 14:34:47 +0100

    add email

---
+++ solartree_crm_lead_email/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "CRM Lead Email",
+    "summary": "Send CRM Lead by email",
+    "version": "17.0.1.0.0",
+    "author":
+        "Salvador Gonzalez (Xtendoo)",
+    "category": "CRM",
+    "license": "AGPL-3",
+    "depends": ["stock", "mail", "crm"],
+    "data": [
+        "views/crm_lead_view.xml",
+        "data/mail_template_data.xml"
+    ],
+    "installable": True,
+}
---
+++ solartree_crm_lead_email/models/crm_lead.py
@@ -0,0 +1,38 @@
+from odoo import _, models, fields
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    all_users_emails = fields.Char(string="All Users Emails", compute="_compute_all_users_emails")
+
+    def _compute_all_users_emails(self):
+        res = self.env['res.users'].search_read([], ['email'])
+        emails = set(r['email'] for r in res if r.get('email'))
+        for record in self:
+            record.all_users_emails = ','.join(emails)
+
+    def action_crm_lead_send_email(self):
+        self.ensure_one()
+        template = self.env.ref("solartree_crm_lead_email.mail_template_data_crm_lead_email", False)
+        compose_form = self.env.ref("mail.email_compose_message_wizard_form")
+        email_values = {
+            'email_to': self.all_users_emails,
+        }
+        ctx = dict(
+            default_model="crm.lead",
+            default_res_ids=self.ids,
+            default_use_template=bool(template),
+            default_template_id=template and template.id or False,
+            default_composition_mode="comment",
+        )
+        return {
+            "name": _("Compose Email"),
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "res_model": "mail.compose.message",
+            "views": [(compose_form.id, "form")],
+            "view_id": compose_form.id,
+            "target": "new",
+            "context": ctx,
+        }
