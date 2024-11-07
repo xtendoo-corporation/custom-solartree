cb88c41bda0f9a95c212b051b2fa1f308693e45f
Gokac9 <theabraham9@gmail.com>
2024-11-07 10:39:27 +0100

    some constraints new fields, translates etc 2

---
+++ solartree_crm_lead_fields/models/crm_leadopportunity_partner.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class CrmLead2OpportunityPartner(models.TransientModel):
+    _inherit = 'crm.lead2opportunity.partner'
+
+    solartree_lead_channel = fields.Many2one(
+        related='lead_id.solartree_lead_channel',
+        comodel_name='crm.lead.channel',
+        string='Lead Channel',
+        readonly=False
+    )
+    solartree_lead_identification = fields.Char(
+        related='lead_id.solartree_lead_identification',
+        string='Lead Identification',
+        readonly=False
+    )
+    solartree_date_request = fields.Date(
+        related='lead_id.solartree_date_request',
+        string='Date Request',
+        readonly=False
+    )
