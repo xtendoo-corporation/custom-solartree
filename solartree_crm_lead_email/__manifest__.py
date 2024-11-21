ee5087c3ddcb8a35f23220ddef74d8eaaa6fb878
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-11-21 14:15:44 +0100

    fix errors

--- solartree_crm_lead_email/__manifest__.py
+++ solartree_crm_lead_email/__manifest__.py
@@ -6,10 +6,8 @@
         "Salvador Gonzalez (Xtendoo)",
     "category": "CRM",
     "license": "AGPL-3",
-    "depends": ["stock", "mail", "crm", "solartree_crm_lead_automatization", "solartree_crm_lead_fields"],
+    "depends": ["crm", "stock", "mail", "solartree_crm_lead_automatization", "solartree_crm_lead_fields"],
     "data": [
-        # "views/crm_lead_view.xml",
-        # 'wizard/crm_lead_to_opportunity_views.xml',
         "data/mail_template_data.xml"
     ],
     "installable": True,
