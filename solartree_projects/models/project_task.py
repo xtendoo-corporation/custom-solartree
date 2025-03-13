8fd315121be936f6d0141a6f79a4a35f5341c9f2
Gokac9 <theabraham9@gmail.com>
2025-03-13 09:51:45 +0100

    sync

--- solartree_projects/models/project_task.py
+++ solartree_projects/models/project_task.py
@@ -1,11 +1,15 @@
-from odoo import models, fields
+import logging
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
 
 class ProjectTaskInherited(models.Model):
     _inherit = 'project.task'
 
-    start_date_planned = fields.Date(
-        string='Planned Start Date'
-    )
-    date_end = fields.Date(
-        string='Date End'
-    )
+    start_date_planned = fields.Date(string='Planned Start Date')
+    date_end = fields.Date(string='Date End')
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        default["message_ids"] = False
+        return super(ProjectTaskInherited, self).copy(default)
